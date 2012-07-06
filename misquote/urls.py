6d34e3be86dd8be16e995160dcf5e194cae7de63
Aria Moraine <ariamoraine@gmail.com>
2012-07-06 15:29:58 -0400

    got it working

--- misquote/urls.py
+++ misquote/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import patterns, include, url
 from misquote.views import index
-
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
