aa4c20f18b0b7bd2a60c039d0247f442499bb98b
Aria Moraine <ariamoraine@gmail.com>
2012-07-07 12:42:21 -0400

    adding longer authors

--- misquote/models.py
+++ misquote/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Author(models.Model):
-	author_name = models.CharField(max_length=100)
+	author_name = models.CharField(max_length=200)
 	
 	def __unicode__(self):
 		return u'Author Name: %s' %(self.author_name)
